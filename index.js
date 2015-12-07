f6b0b34338addc361910995ce4850c61b432ac11
Anthony Donaldson <donaldsa18@up.edu>
2015-12-07 12:59:48 -0800

    Implemented compression in app.js to decrease page load times.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+var compression = require('compression');
+//var compression = require('minify');
+
+/*var compressedFiles = ["excanvas.js"]; //["excanvas.js","handler.js","helper.js","index.html","style.css","viz.js"];
+
+var errorFunction = function(error, name) {
+    console.log(error || name);
+};
+
+for(var i=0;i<compressedFiles.length;i++) {
+
+}
+minify("public\"+compressedFiles[i], errorFunction);*/
+
+var http = require('http');  
+var https = require('https');
+http.globalAgent.maxSockets = Infinity;  
+https.globalAgent.maxSockets = Infinity;  
+
 var express = require('express');
 var app = express();
 
+app.use(compression());
+
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static('public'));
+app.use(express.static('public', { maxAge: 31557600 }));
 
 
 app.listen(app.get('port'), function() {
