e4da31e657008b0639848a146acaab72277254f0
Anthony Donaldson <donaldsa18@up.edu>
2015-12-06 19:23:23 -0800

    Used minify and express's compression to optimize page loads.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var minify = require('express-minify');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.compress());
+app.use(minify());
 app.use(express.static('public'));
+
 
 app.listen(app.get('port'), function() {
     console.log("Node is running at: " + app.get('port'));    
